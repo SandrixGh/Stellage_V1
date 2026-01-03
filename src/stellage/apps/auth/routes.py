bfe1b7821d1ad50310835eeb0e7a5755b48370e0
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-03 20:22:46 +0300

    add confirm_registration method to routes

--- src/stellage/apps/auth/routes.py
+++ src/stellage/apps/auth/routes.py
@@ -11,7 +11,7 @@ auth_router = APIRouter(
 )
 
 @auth_router.post(
-    "/register",
+    path="/register",
     response_model=UserReturnData,
     status_code=status.HTTP_201_CREATED,
 )
@@ -23,3 +23,19 @@ async def registration(
     ]
 ) -> UserReturnData:
     return await service.register_user(user)
+
+
+@auth_router.get(
+    path="/register_confirm/",
+    response_model=dict[str, str],
+    status_code=status.HTTP_200_OK,
+)
+async def confirm_registration(
+    token: str,
+    service: Annotated[
+        UserService,
+        Depends(UserService)
+    ]
+) -> dict[str, str]:
+    await service.confirm_user(token=token)
+    return {"message": "User confirmed successfully"}
