f965d1e41da49a9f75a60ffd2a779d42ffe4d90e
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-03 19:48:36 +0300

    add celery_config

---
+++ src/stellage/core/celery_config.py
@@ -0,0 +1,15 @@
+from celery import Celery
+from stellage.core.settings import settings
+
+celery_app = Celery(
+    main="stellage",
+    broker=settings.redis_settings.redis_url,
+    backend=settings.redis_settings.redis_url,
+    include=["stellage.apps.auth.tasks"]
+)
+
+celery_app.autodiscover_tasks(
+    packages=["stellage.apps"],
+    force=True
+)
+
