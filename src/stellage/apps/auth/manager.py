4dd1f51b85b5fa596c0ad962d28a5c36d39968df
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-03 13:45:02 +0300

    writing user manager and first create_user method

---
+++ src/stellage/apps/auth/manager.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy import insert
+from sqlalchemy.exc import IntegrityError
+
+from stellage.apps.auth.schemas import CreateUser, UserReturnData
+from stellage.core.core_dependencies.db_dependency import DBDependency
+from stellage.database.models import User
+
+
+class UserManager:
+    def __init__(
+        self,
+        db: DBDependency = Depends(DBDependency)
+    ) -> None:
+        self.db = db
+        self.model = User
+
+    async def create_user(
+        self,
+        user: CreateUser
+    ) -> UserReturnData:
+        async with self.db.db_session() as session:
+            query = insert(self.model).values(**user.model_dump()).returning(self.model)
+
+            try:
+                result = await session.execute(query)
+
+            except IntegrityError:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="User already exists"
+                )
+
+            await session.commit()
+            user_data = result.scalar_one()
+            return UserReturnData(**user_data.__dict__)
