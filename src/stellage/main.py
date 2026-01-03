d67e0af1f148734677b43b2dbe09dc999cce7b8d
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-03 14:29:43 +0300

    add apps_router to main app

--- src/stellage/main.py
+++ src/stellage/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from stellage.apps import apps_router
+
 app = FastAPI()
+app.include_router(
+    router=apps_router,
+)
 
 def start():
     uvicorn.run("stellage.main:app", reload=True)
