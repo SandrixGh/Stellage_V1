e03aedd57dc210afa93c23c496ec6814879606b7
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-03 13:36:30 +0300

    user schemas

---
+++ src/stellage/apps/auth/schemas.py
@@ -0,0 +1,32 @@
+import datetime
+import uuid
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, StringConstraints
+
+
+class GetUserByID(BaseModel):
+    id: uuid.UUID | str
+
+
+class GetUserByEmail(BaseModel):
+    email: EmailStr
+
+
+class RegisterUser(GetUserByEmail):
+    password: Annotated[str, StringConstraints(
+        min_length=8,
+        max_length=128,
+    )]
+
+
+class CreateUser(GetUserByEmail):
+    hashed_password: str
+
+
+class UserReturnData(GetUserByID, GetUserByEmail):
+    is_active: bool = False
+    is_verified: bool = False
+    is_superuser: bool = False
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
