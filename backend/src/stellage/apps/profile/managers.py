222d897165ea7713c53980c4492e3647b148dcdc
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-24 21:29:50 +0300

    add get_user_hashed_password, store_confirmation_code, get_new_email_by_confirmation_code, remove_confirmation_code methods to ProfileManager

--- backend/src/stellage/apps/profile/managers.py
+++ backend/src/stellage/apps/profile/managers.py
@@ -1,8 +1,9 @@
 import uuid
 
 from fastapi import Depends
-from sqlalchemy.orm.sync import update
+from sqlalchemy import update, select
 
+from stellage.apps.profile.schemas import ConfirmationCodeRequest
 from stellage.core.core_dependencies.db_dependency import DBDependency
 from stellage.core.core_dependencies.redis_dependency import RedisDependency
 from stellage.database.models import User
@@ -24,7 +25,7 @@ class ProfileManager:
         **kwargs,
     ) -> None:
         async with self.db.db_session() as session:
-            query =(
+            query = (
                 update(
                     self.user_model
                 )
@@ -33,3 +34,45 @@ class ProfileManager:
             )
             await session.execute(query)
             await session.commit()
+
+
+    async def get_user_hashed_password(
+        self,
+        user_id: uuid.UUID | None
+    ) -> str | None:
+        async with self.db.db_session() as session:
+            query = (
+                select(
+                    self.user_model.hashed_password
+                )
+                .where(self.user_model.id == user_id)
+            )
+            result = await session.execute(query)
+            return result.scalar()
+
+
+    async def store_confirmation_code(
+        self,
+        confirmation_code_request: ConfirmationCodeRequest
+    ) -> None:
+        async with self.redis.get_client() as client:
+            return await client.set(
+                f"{confirmation_code_request.confirmation_code}",
+                confirmation_code_request.email
+            )
+
+
+    async def get_new_email_by_confirmation_code(
+        self,
+        confirmation_code: str,
+    ) -> str | None:
+        async with self.redis.get_client() as client:
+            return await client.get(f"{confirmation_code}")
+
+
+    async def remove_confirmation_code(
+        self,
+        confirmation_code: str
+    ) -> None:
+        async with self.redis.get_client() as client:
+            return await client.delete(f"{confirmation_code}")
