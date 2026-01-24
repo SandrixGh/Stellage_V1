2112f75631cc9c4ab9263b41d5406e093d8ce5c4
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-24 22:16:36 +0300

    refactoring

--- backend/src/stellage/apps/profile/tasks.py
+++ backend/src/stellage/apps/profile/tasks.py
@@ -5,22 +5,22 @@ import smtplib
 from celery import shared_task
 from jinja2 import Environment, FileSystemLoader
 
-from stellage.apps.profile.schemas import ConfirmationCodeRequest
 from stellage.core.settings import settings
 
 
 @shared_task
 def send_confirmation_code(
-    confirmation_code_request: ConfirmationCodeRequest,
+    to_email: str,
+    confirmation_code: str,
 ) -> None:
     env = Environment(loader=FileSystemLoader(settings.templates_dir))
     template = env.get_template("confirmation_code.html")
-    html_content = template.render(confirmation_code=confirmation_code_request.confirmation_code)
+    html_content = template.render(confirmation_code=confirmation_code)
 
     message = EmailMessage()
     message.add_alternative(html_content, subtype="html")
     message["From"] = settings.email_settings.email_username
-    message["To"] = confirmation_code_request.email
+    message["To"] = to_email
     message["Subject"] = "Подтверждение новой почты"
 
     with smtplib.SMTP_SSL(
