d0b784e7474c9db1d5cd7b2a34c923f8656b5ba0
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-24 22:05:58 +0300

    add profile_router to main

--- backend/src/stellage/apps/__init__.py
+++ backend/src/stellage/apps/__init__.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 
 from stellage.apps.auth.routes import auth_router
+from stellage.apps.profile.routes import profile_router
 
 apps_router = APIRouter(
     prefix="/api.v1",
 )
 apps_router.include_router(
     router=auth_router,
+)
+apps_router.include_router(
+    router=profile_router
 )
