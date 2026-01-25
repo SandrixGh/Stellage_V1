7a22695000f3cf59579bc58afcf0747c2c171106
Alexander Karpov <alexanderak.0501@gmail.com>
2026-01-25 20:33:59 +0300

    create auth store

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import type { UserVerifySchema } from "../types/auth";
+import { api } from "../api/instance";
+
+interface AuthState {
+    user: UserVerifySchema | null;
+    isAuthenticated: boolean;
+    isInitialized: boolean;
+
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+    user: null,
+    isAuthenticated: false,
+    isInitialized: false,
+
+    checkAuth: async () => {
+        try {
+            const res = await api.get<UserVerifySchema>("/auth/get-user");
+            set({user: res.data, isAuthenticated: true, isInitialized: true});
+        } catch {
+            set({ user: null, isAuthenticated: false, isInitialized: true });
+        }
+    },
+
+    login: async (email, password) => {
+        await api.post("/auth/login", {email, password});
+        const res = await api.get<UserVerifySchema>("/auth/get-user");
+        set({user: res.data, isAuthenticated: true})
+    },
+
+    logout: async () => {
+        await api.get('/auth/logout');
+        set({ user: null, isAuthenticated: false });
+    },
+}))
+
