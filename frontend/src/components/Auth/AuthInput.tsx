0932c2f83c12038e9d32a16ddd3244591d833f19
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-01 21:46:01 +0300

    add showing password and refactoring

--- frontend/src/components/Auth/AuthInput.tsx
+++ frontend/src/components/Auth/AuthInput.tsx
@@ -1,16 +1,34 @@
 import type { AuthInputProps } from "../../types/Auth/AuthInput"
-import "../../pages/Auth.css"
+import "../../pages/Auth/Auth.css"
+import { useState } from "react";
 
 export const AuthInput = ({ field, setField, label, type }: AuthInputProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const isPasswordField = type === "password";
+    const currentType = isPasswordField && showPassword ? "text" : type;
+
     return (
         <div className="form-group">
             <label>{label}</label>
-            <input
-                type={type}
-                value={field}
-                onChange={(e) => setField(e.target.value)}
-                required
-            />
+            <div className="input-wrapper">
+                <input
+                    type={currentType}
+                    value={field}
+                    onChange={(e) => setField(e.target.value)}
+                    required
+                />
+                {type === "password" && (
+                    <button
+                        type="button"
+                        className={`password-toggle ${showPassword ? "active" : ""}`}
+                        onClick={() => setShowPassword(!showPassword)}
+                        tabIndex={-1}
+                    >
+                        {showPassword ? "✓" : "○"}
+                    </button>
+                )}
+            </div>
         </div>
     )
 }
