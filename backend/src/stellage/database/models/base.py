b6938fe37b3525e42fd611190748a48ead67d8f9
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-08 22:11:07 +0300

    some changes about table naming

--- backend/src/stellage/database/models/base.py
+++ backend/src/stellage/database/models/base.py
@@ -4,4 +4,4 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr
 class Base(DeclarativeBase):
     @declared_attr
     def __tablename__(cls):
-        return cls.__name__.lower()
+        return f"{cls.__name__.lower()}s"
