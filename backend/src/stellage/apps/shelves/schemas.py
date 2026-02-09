95738ff507ea19dcd54b04896a004bd23239e704
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-09 20:11:40 +0300

    create schemas to shelf

---
+++ backend/src/stellage/apps/shelves/schemas.py
@@ -0,0 +1,52 @@
+import datetime
+import uuid
+from typing import Annotated
+
+from pydantic import BaseModel, StringConstraints, ConfigDict
+
+
+class GetShelfByID(BaseModel):
+    id: uuid.UUID
+
+
+class GetShelfByTitle(BaseModel):
+    title: Annotated[
+        str,
+        StringConstraints(
+            min_length=3,
+            max_length=100,
+            strip_whitespace=True,
+        )
+    ] = "Мой стеллаж"
+
+
+class ShelfOwner(BaseModel):
+    user_id: uuid.UUID
+
+
+class ShelfFlags(BaseModel):
+    is_main: bool = False
+    is_public: bool = True
+
+
+class CreateShelf(GetShelfByTitle, ShelfFlags):
+    pass
+
+
+class UpdateShelf(BaseModel):
+    title: Annotated[
+        str | None,
+        StringConstraints(
+            min_length=3,
+            max_length=100,
+            strip_whitespace=True,
+        )
+    ] = None
+    is_public: bool | None = None
+
+
+class ShelfReturnData(GetShelfByID, GetShelfByTitle, ShelfOwner, ShelfFlags):
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+    model_config = ConfigDict(from_attributes=True)
