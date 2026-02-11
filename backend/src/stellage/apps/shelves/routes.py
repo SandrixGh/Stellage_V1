032918a3888b734a10803b7eb514ade552e82f02
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-11 19:12:29 +0300

    create routes for shelf

---
+++ backend/src/stellage/apps/shelves/routes.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
+
+from stellage.apps.auth.depends import get_current_user
+from stellage.apps.auth.schemas import UserVerifySchema
+from stellage.apps.shelves.schemas import ShelfReturnData, CreateShelf
+from stellage.apps.shelves.services import ShelfService
+
+router = APIRouter(
+    prefix="/shelf",
+    tags=["Shelf"]
+)
+
+@router.post(
+    path="/create-shelf",
+    status_code=status.HTTP_200_OK,
+    response_model=ShelfReturnData
+)
+async def create_shelf(
+    user: Annotated[
+        UserVerifySchema,
+        Depends(get_current_user)
+    ],
+    service: Annotated[
+        ShelfService,
+        Depends(ShelfService)
+    ],
+    shelf: CreateShelf
+) -> ShelfReturnData:
+    return await service.create_shelf(
+        user=user,
+        shelf=shelf,
+    )
