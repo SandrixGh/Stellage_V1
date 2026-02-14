9a33a617363881c061c289f989b423148a3b374c
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-14 21:37:51 +0300

    changed logic of create/delete shelves in redis and creating shelf

--- backend/src/stellage/apps/shelves/managers.py
+++ backend/src/stellage/apps/shelves/managers.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from sqlalchemy import insert, select, update
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from stellage.apps.shelves.schemas import CreateShelf, ShelfReturnData, GetShelfByTitle, GetShelfIdAndTitleAndOwner
 from stellage.core.core_dependencies.db_dependency import DBDependency
@@ -28,12 +29,19 @@ class ShelfManager:
         self.redis = redis
         self.model = Shelf
 
+
     async def create_shelf(
         self,
         user_id: uuid.UUID,
         shelf: CreateShelf
     ) -> ShelfReturnData:
         async with self.db.db_session() as session:
+            if shelf.is_main:
+                await self.reset_main_shelf(
+                    user_id=user_id,
+                    session=session,
+                )
+
             data = shelf.model_dump()
             data["user_id"] = user_id
 
@@ -51,6 +59,9 @@ class ShelfManager:
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Shelf already exist"
                 )
+            except Exception as e:
+                await session.rollback()
+                raise e
 
 
     async def get_shelves(
@@ -67,30 +78,54 @@ class ShelfManager:
             return [ShelfReturnData.model_validate(shelf) for shelf in shelves]
 
 
+    async def reset_main_shelf(
+        self,
+        user_id: uuid.UUID | str ,
+        session: AsyncSession
+    ) -> None:
+        query = (
+            update(self.model)
+            .where(
+                self.model.user_id == user_id,
+                self.model.is_main == True,
+            )
+            .values(is_main=False)
+        )
+
+        await session.execute(query)
+
+
     async def store_shelf_to_redis(
         self,
         shelf: ShelfReturnData,
     ) -> None:
         async with self.redis.get_client() as client:
-            key = f"{shelf.user_id}:{shelf.id}"
+            key = f"shelf:{shelf.user_id}:{shelf.id}"
             return await client.set(key, pack_to_json(shelf), ex=3600)
 
 
-
-    async def reset_main_shelf(
+    async def store_main_shelf_to_redis(
         self,
-        user_id: uuid.UUID | str ,
+        shelf: ShelfReturnData,
     ) -> None:
-        async with self.db.db_session() as session:
-            query = (
-                update(self.model)
-                .where(
-                    self.model.user_id == user_id,
-                    self.model.is_main == True,
-                )
-                .values(is_main=False)
-            )
-            await session.execute(query)
-            await session.commit()
+        async with self.redis.get_client() as client:
+            key = f"main_shelf:{shelf.user_id}"
+            return await client.set(key, pack_to_json(shelf), ex=3600)
 
 
+    async def delete_shelf_cache(
+        self,
+        shelf: ShelfReturnData,
+    ) -> None:
+        async with self.redis.get_client() as client:
+            key = f"shelf:{shelf.user_id}:{shelf.id}"
+            return await client.delete(key)
+
+
+    async def delete_main_shelf_cache(
+        self,
+        user_id: uuid.UUID | str
+    ) -> None:
+        async with self.redis.get_client() as client:
+            key = f"main_shelf:{user_id}"
+            return await client.delete(key)
