f9e381aa7c485f07cf24e970ac87adc7bb51b50c
Alexander Karpov <alexanderak.0501@gmail.com>
2026-02-14 17:10:19 +0300

    changes at user and shelf models

--- backend/src/stellage/database/models/user.py
+++ backend/src/stellage/database/models/user.py
@@ -1,12 +1,18 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from stellage.database.mixins.id_mixins import IDMixin
 from stellage.database.mixins.timestamp_mixins import TimestampMixin
 from stellage.database.models.base import Base
 
+if TYPE_CHECKING:
+    from stellage.database.models.shelf import Shelf
 
 class User(IDMixin, TimestampMixin, Base):
+    __tablename__ = "users"
+
     email: Mapped[str] = mapped_column(
         String(100),
         unique=True,
@@ -31,4 +37,9 @@ class User(IDMixin, TimestampMixin, Base):
         Boolean,
         default=False,
         nullable=False,
+    )
+    shelves: Mapped[list["Shelf"]] = relationship(
+        "Shelf",
+        back_populates="owner",
+        cascade="all, delete-orphan",
     )
