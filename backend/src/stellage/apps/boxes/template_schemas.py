ab418c8e770157263069c90b2d6d0062bbc6686a
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-09 19:23:01 +0300

    add template and instance schemas to boxes

---
+++ backend/src/stellage/apps/boxes/template_schemas.py
@@ -0,0 +1,35 @@
+import datetime
+import uuid
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict
+
+from stellage.database.enums.box_rarity import BoxRarity
+from stellage.database.enums.currency import CurrencyEnum
+
+
+class GetBoxTemplateById(BaseModel):
+    id: uuid.UUID
+
+
+class BoxTemplateTimeStamps(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+
+
+class BoxTemplateBase(BaseModel):
+    title: str
+    description: str | None = None
+    price: Decimal
+    currency: CurrencyEnum = CurrencyEnum.RUB
+    rarity: BoxRarity = BoxRarity.COMMON
+
+
+class BoxTemplateCreate(BoxTemplateBase):
+    pass
+
+
+class BoxTemplateReturn(GetBoxTemplateById, BoxTemplateBase, BoxTemplateTimeStamps):
+    model_config = ConfigDict(
+        from_attributes=True
+    )
