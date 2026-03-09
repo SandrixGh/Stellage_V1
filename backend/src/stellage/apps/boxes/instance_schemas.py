55673041ad0fcf02b379611c19a03b323be9cde3
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-09 19:23:01 +0300

    add template and instance schemas to boxes

---
+++ backend/src/stellage/apps/boxes/instance_schemas.py
@@ -0,0 +1,64 @@
+import datetime
+import uuid
+
+from pydantic import BaseModel, ConfigDict
+
+from stellage.database.enums.box_sealing import SealingEnum
+from stellage.database.enums.verification import VerifyEnum
+from stellage.database.enums.visibility import VisibilityEnum
+
+
+class GetBoxInstanceById(BaseModel):
+    id: uuid.UUID
+
+
+class GetShelfId(BaseModel):
+    shelf_id: uuid.UUID | None
+
+
+class GetOwnerId(BaseModel):
+    user_id: uuid.UUID
+
+
+class GetTemplateId(BaseModel):
+    template_id: uuid.UUID
+
+class GetParentsIds(
+    GetShelfId,
+    GetOwnerId,
+    GetTemplateId,
+):
+    pass
+
+
+class BoxInstanceBase(BaseModel):
+    is_sealed: SealingEnum = SealingEnum.SEALED
+    is_public: VisibilityEnum = VisibilityEnum.PRIVATE
+    is_verified: VerifyEnum = VerifyEnum.NOT_VERIFIED
+    content: dict | None = None
+
+
+class BoxInstanceTimeStamps(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+
+
+class BoxInstanceReturn(
+    GetBoxInstanceById,
+    GetParentsIds,
+    BoxInstanceBase,
+    BoxInstanceTimeStamps
+):
+    serial_number: int
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BoxInstanceUpdate(BaseModel):
+    shelf_id: uuid.UUID | None = None
+    is_sealed: SealingEnum | None = None
+    is_public: VisibilityEnum | None = None
+    content: dict | None = None
+
+
+class BoxInstanceCreate(BoxInstanceBase, GetShelfId, GetOwnerId):
+    pass
