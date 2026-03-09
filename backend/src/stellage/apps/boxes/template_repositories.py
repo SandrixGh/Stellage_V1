37132b7641ad28761748df42809bc3de2d896c14
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-09 22:08:21 +0300

    created BoxTemplateRepository

---
+++ backend/src/stellage/apps/boxes/template_repositories.py
@@ -0,0 +1,92 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from sqlalchemy import insert, select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
+from starlette import status
+
+from stellage.apps.boxes.template_schemas import BoxTemplateCreate, BoxTemplateReturn, BoxTemplateReturnWithInstances
+from stellage.core.core_dependencies.db_dependency import DBDependency
+from stellage.database.models import BoxTemplate
+
+
+class BoxTemplateRepository:
+    def __init__(
+        self,
+        db: Annotated[
+            DBDependency,
+            Depends(DBDependency)
+        ]
+    ) -> None:
+        self.db = db
+        self.template_model = BoxTemplate
+
+
+    async def create_template(
+        self,
+        data: BoxTemplateCreate
+    ) -> BoxTemplateReturn:
+        async with self.db.db_session() as session:
+            query = (
+                insert(self.template_model)
+                .values(**data)
+                .returning(self.template_model)
+            )
+            try:
+                result = await session.execute(query)
+                template = result.scalar_one()
+                await session.commit()
+                return BoxTemplateReturn.model_validate(template)
+
+            except IntegrityError:
+                await session.rollback()
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Template already exist"
+                )
+
+            except Exception as e:
+                await session.rollback()
+                raise e
+
+
+    async def get_templates(
+        self,
+    ) -> list[BoxTemplateReturn]:
+        async with self.db.db_session() as session:
+            query = (
+                select(self.template_model)
+            )
+            result = await session.execute(query)
+            return[
+                BoxTemplateReturn.model_validate(template)
+                for template in result.scalars()
+            ]
+
+
+    async def get_template_with_instances(
+        self,
+        template_id: uuid.UUID
+    ) -> BoxTemplateReturnWithInstances:
+        async with self.db.db_session() as session:
+            query = (
+                select(
+                    self.template_model
+                )
+                .options(joinedload(self.template_model.instances))
+                .where(self.template_model.id == template_id)
+            )
+
+            result = await session.execute(query)
+            template = result.scalar_one_or_none()
+
+            if not template:
+                await session.rollback()
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Template not found"
+                )
+
+            return BoxTemplateReturnWithInstances.model_validate(template)
