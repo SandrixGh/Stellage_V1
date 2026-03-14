8de0c6e83c2820ee7623e685fa4079d82ec19e41
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-14 12:45:38 +0300

    created TemplateManager and InstanceManager

---
+++ backend/src/stellage/apps/boxes/templates/managers.py
@@ -0,0 +1,42 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Depends
+
+from stellage.apps.boxes.templates.repositories import BoxTemplateRepository
+from stellage.apps.boxes.templates.schemas import BoxTemplateCreate, BoxTemplateReturn
+
+
+class TemplateManager:
+    def __init__(
+        self,
+        repository: Annotated[
+            BoxTemplateRepository,
+            Depends(BoxTemplateRepository),
+        ]
+    ) -> None:
+        self.repository = repository
+
+
+    async def create_template(
+        self,
+        data: BoxTemplateCreate,
+    ) -> BoxTemplateReturn:
+        return await self.repository.create_template(
+            data=data,
+        )
+
+
+    async def get_templates(
+        self
+    ) -> list[BoxTemplateReturn]:
+        return await self.repository.get_templates()
+
+
+    async def get_template_with_instances(
+        self,
+        template_id: uuid.UUID,
+    ) -> BoxTemplateReturn:
+        return await self.repository.get_template_with_instances(
+            template_id=template_id,
+        )
