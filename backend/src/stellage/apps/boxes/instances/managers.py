0091b91f95dac74ee6cf5a493c878c1a444e31da
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-14 12:45:38 +0300

    created TemplateManager and InstanceManager

---
+++ backend/src/stellage/apps/boxes/instances/managers.py
@@ -0,0 +1,73 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Depends
+
+from stellage.apps.boxes.instances.repositories import BoxInstanceRepository
+from stellage.apps.boxes.instances.schemas import BoxInstanceCreate, BoxInstanceReturn
+
+
+class InstanceManager:
+    def __init__(
+        self,
+        repository: Annotated[
+            BoxInstanceRepository,
+            Depends(BoxInstanceRepository)
+        ],
+    ) -> None:
+        self.repository = repository
+
+
+    async def create_instance(
+        self,
+        user_id: uuid.UUID,
+        data: BoxInstanceCreate
+    ) -> BoxInstanceReturn:
+        return await self.repository.create_instance(
+            user_id=user_id,
+            data=data,
+        )
+
+
+    async def move_to_shelf(
+        self,
+        user_id: uuid.UUID,
+        instance_id: uuid.UUID,
+        shelf_id: uuid.UUID,
+    ) -> BoxInstanceReturn:
+        return await self.repository.move_to_shelf(
+            user_id=user_id,
+            shelf_id=shelf_id,
+            instance_id=instance_id,
+        )
+
+
+    async def get_instances(
+        self,
+        user_id: uuid.UUID,
+    ) -> list[BoxInstanceReturn]:
+        return await self.repository.get_box_instances(
+            user_id=user_id,
+        )
+
+
+    async def get_instance_by_id(
+        self,
+        user_id: uuid.UUID,
+        instance_id: uuid.UUID,
+    ) -> BoxInstanceReturn:
+        return await self.get_instance_by_id(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
+
+
+    async def delete_instance(
+        self,
+        user_id: uuid.UUID,
+        instance_id: uuid.UUID,
+    ) -> None:
+        return await self.delete_instance(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
