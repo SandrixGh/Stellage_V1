b1d793f2f2e960791e233f221b3ec6542162a23f
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-15 18:23:55 +0300

    added get_main_shelf_with_boxes and get_shelf_with_boxes methods in ShelfService and dependencies

--- backend/src/stellage/apps/shelves/dependecies.py
+++ backend/src/stellage/apps/shelves/dependecies.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 
 from stellage.apps.auth.depends import get_current_user
 from stellage.apps.auth.schemas import UserVerifySchema
-from stellage.apps.shelves.schemas import ShelfReturnData
+from stellage.apps.shelves.schemas import ShelfReturnData, ShelfWithBoxInstances
 from stellage.apps.shelves.services import ShelfService
 
 
@@ -19,3 +19,16 @@ async def get_current_main_shelf(
     ]
 ) -> ShelfReturnData:
     return await service.get_main_shelf(user=user)
+
+
+async def get_current_main_shelf_with_boxes(
+    user: Annotated[
+        UserVerifySchema,
+        Depends(get_current_user)
+    ],
+    service: Annotated[
+        ShelfService,
+        Depends(ShelfService)
+    ]
+) -> ShelfWithBoxInstances:
+    return await service.get_main_shelf_with_boxes(user=user)
