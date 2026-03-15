9019578949e4a03e0fb86b80c108dd9f5b6fe99f
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-15 18:00:37 +0300

    added get_shelf_with_instances and store_shelf_full methods to cache_manager

--- backend/src/stellage/apps/shelves/cache_managers.py
+++ backend/src/stellage/apps/shelves/cache_managers.py
@@ -2,7 +2,7 @@ import uuid
 from typing import Annotated
 from fastapi import Depends
 
-from stellage.apps.shelves.schemas import ShelfReturnData
+from stellage.apps.shelves.schemas import ShelfReturnData, ShelfWithBoxInstances
 from stellage.apps.shelves.utils import unpack_from_json, pack_to_json
 from stellage.core.core_dependencies.redis_dependency import RedisDependency
 
@@ -58,3 +58,23 @@ class ShelfCacheManager:
     async def refresh_main_shelf(self, user_id: uuid.UUID, shelf: ShelfReturnData) -> None:
         await self.delete_main_shelf(user_id=user_id)
         await self.store_main_shelf(shelf=shelf)
+
+
+    async def get_shelf_with_instances(
+        self,
+        user_id: uuid.UUID,
+        shelf_id: uuid.UUID
+    ) -> ShelfWithBoxInstances | None:
+        async with self.redis.get_client() as client:
+            key = f"shelf_full:{user_id}:{shelf_id}"
+            data = await client.get(key)
+            return unpack_from_json(data, ShelfWithBoxInstances) if data else None
+
+
+    async def store_shelf_full(
+        self,
+        shelf: ShelfWithBoxInstances,
+    ) -> None:
+        async with self.redis.get_client() as client:
+            key = f"shelf_full:{shelf.user_id}:{shelf.id}"
+            await client.set(key, pack_to_json(shelf), ex=3600)
