ae1e97c43ca399a59b3461cb4ec8f009a47d4cd0
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-21 21:42:55 +0300

    updated ShelfCacheManager: deleted dividing keys into "main_shelf:..." and "shelf:..."

--- backend/src/stellage/apps/shelves/cache_managers.py
+++ backend/src/stellage/apps/shelves/cache_managers.py
@@ -1,10 +1,12 @@
 import uuid
 from typing import Annotated
+
 from fastapi import Depends
 
-from stellage.apps.shelves.schemas import ShelfReturnData, ShelfWithBoxInstances
 from backend.utils.utils import unpack_from_json, pack_to_json
+from stellage.apps.shelves.schemas import ShelfWithBoxInstances, ShelfReturnData
 from stellage.core.core_dependencies.redis_dependency import RedisDependency
+
 
 class ShelfCacheManager:
     def __init__(
@@ -12,88 +14,74 @@ class ShelfCacheManager:
         redis: Annotated[
             RedisDependency,
             Depends(RedisDependency)
-        ],
+        ]
     ) -> None:
         self.redis = redis
 
 
-    async def get_main_shelf(self, user_id: uuid.UUID) -> ShelfReturnData | None:
-        async with self.redis.get_client() as client:
-            key = f"main_shelf:{user_id}"
-            data = await client.get(key)
-            return unpack_from_json(data, ShelfReturnData) if data else None
-
-
-    async def get_shelf(self, user_id: uuid.UUID, shelf_id: uuid.UUID) -> ShelfReturnData | None:
-        async with self.redis.get_client() as client:
-            key = f"shelf:{user_id}:{shelf_id}"
-            data = await client.get(key)
-            return unpack_from_json(data, ShelfReturnData) if data else None
-
-
-    async def store_shelf(self, shelf: ShelfReturnData) -> None:
-        async with self.redis.get_client() as client:
-            key = f"shelf:{shelf.user_id}:{shelf.id}"
-            await client.set(key, pack_to_json(shelf), ex=3600)
-
-
-    async def store_main_shelf(self, shelf: ShelfReturnData) -> None:
-        async with self.redis.get_client() as client:
-            key = f"main_shelf:{shelf.user_id}"
-            await client.set(key, pack_to_json(shelf), ex=3600)
-
-
-    async def delete_shelf(self, shelf_id: uuid.UUID, user_id: uuid.UUID) -> None:
-        async with self.redis.get_client() as client:
-            key = f"shelf:{user_id}:{shelf_id}"
-            await client.delete(key)
-
-
-    async def delete_main_shelf(self, user_id: uuid.UUID) -> None:
-        async with self.redis.get_client() as client:
-            key = f"main_shelf:{user_id}"
-            await client.delete(key)
-
-
-    async def refresh_main_shelf(self, user_id: uuid.UUID, shelf: ShelfReturnData) -> None:
-        await self.delete_main_shelf(user_id=user_id)
-        await self.store_main_shelf(shelf=shelf)
-
-
-    async def get_shelf_with_boxes(
+    def _get_key(
         self,
         user_id: uuid.UUID,
-        shelf_id: uuid.UUID
-    ) -> ShelfWithBoxInstances | None:
-        async with self.redis.get_client() as client:
-            key = f"shelf_full:{user_id}:{shelf_id}"
-            data = await client.get(key)
-            return unpack_from_json(data, ShelfWithBoxInstances) if data else None
+        shelf_id: uuid.UUID,
+        is_full: bool = False
+    ) -> str:
+        prefix = "shelf_full" if is_full else "shelf"
+        return f"{prefix}:{user_id}:{shelf_id}"
 
 
-    async def store_shelf_full(
-        self,
-        shelf: ShelfWithBoxInstances,
-    ) -> None:
-        async with self.redis.get_client() as client:
-            key = f"shelf_full:{shelf.user_id}:{shelf.id}"
-            await client.set(key, pack_to_json(shelf), ex=3600)
-
-
-    async def get_main_shelf_with_boxes(
+    async def get_shelf(
         self,
         user_id: uuid.UUID,
-    ) -> ShelfWithBoxInstances | None:
+        shelf_id: uuid.UUID,
+        is_full: bool,
+    ):
         async with self.redis.get_client() as client:
-            key = f"main_shelf_full:{user_id}"
+            key = self._get_key(
+                user_id=user_id,
+                shelf_id=shelf_id,
+                is_full=is_full,
+            )
             data = await client.get(key)
-            return unpack_from_json(data, ShelfWithBoxInstances) if data else None
+            schema = (
+                ShelfWithBoxInstances if is_full
+                else ShelfReturnData
+            )
+            return unpack_from_json(data, schema) if data else None
 
 
-    async def store_main_shelf_full(
+    async def store_shelf(
         self,
-        shelf: ShelfWithBoxInstances,
+        shelf: ShelfReturnData | ShelfWithBoxInstances
     ) -> None:
         async with self.redis.get_client() as client:
-            key = f"main_shelf_full:{shelf.user_id}"
+            is_full = isinstance(shelf, ShelfWithBoxInstances)
+            key = self._get_key(
+                user_id=shelf.user_id,
+                shelf_id=shelf.id,
+                is_full=is_full
+            )
             await client.set(key, pack_to_json(shelf), ex=3600)
+
+
+    async def delete_shelf(
+        self,
+        user_id: uuid.UUID,
+        shelf_id: uuid.UUID,
+    ) -> None:
+        async with self.redis.get_client() as client:
+            await client.delete(
+                self._get_key(
+                    user_id=user_id,
+                    shelf_id=shelf_id,
+                    is_full=True,
+                )
+            )
+            await client.delete(
+                self._get_key(
+                    user_id=user_id,
+                    shelf_id=shelf_id,
+                    is_full=False,
+                )
+            )
+
+
