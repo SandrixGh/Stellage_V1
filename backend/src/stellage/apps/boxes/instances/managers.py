86e0757259b6465ba4e2c142f88e95366191d3db
Alexander Karpov <alexanderak.0501@gmail.com>
2026-03-22 09:54:12 +0300

    redesigned InstanceManager

--- backend/src/stellage/apps/boxes/instances/managers.py
+++ backend/src/stellage/apps/boxes/instances/managers.py
@@ -3,8 +3,10 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from stellage.apps.boxes.instances.cache_managers import InstanceCacheManager
 from stellage.apps.boxes.instances.repositories import BoxInstanceRepository
 from stellage.apps.boxes.instances.schemas import BoxInstanceCreate, BoxInstanceReturn, BoxInstanceWithTemplate
+from stellage.apps.shelves.cache_managers import ShelfCacheManager
 
 
 class InstanceManager:
@@ -14,7 +16,17 @@ class InstanceManager:
             BoxInstanceRepository,
             Depends(BoxInstanceRepository)
         ],
+        instance_cache_manager: Annotated[
+            InstanceCacheManager,
+            Depends(InstanceCacheManager)
+        ],
+        shelf_cache_manager: Annotated[
+            ShelfCacheManager,
+            Depends(ShelfCacheManager)
+        ]
     ) -> None:
+        self.shelf_cache_manager = shelf_cache_manager
+        self.instance_cache_manager = instance_cache_manager
         self.repository = repository
 
 
@@ -22,11 +34,46 @@ class InstanceManager:
         self,
         user_id: uuid.UUID,
         data: BoxInstanceCreate
-    ) -> BoxInstanceReturn:
-        return await self.repository.create_instance(
+    ) -> BoxInstanceWithTemplate:
+        instance = await self.repository.create_instance(
             user_id=user_id,
             data=data,
         )
+
+        if data.shelf_id:
+            await self.shelf_cache_manager.delete_shelf(
+                user_id=instance.user_id,
+                shelf_id=instance.shelf_id,
+            )
+
+        await self.instance_cache_manager.store_instance(
+            instance=instance,
+        )
+
+        return instance
+
+
+    async def refresh_old_shelf(
+        self,
+        user_id: uuid.UUID,
+        instance_id: uuid.UUID,
+    ) -> None:
+        instance = await self.instance_cache_manager.get_instance(
+            instance_id=instance_id,
+            user_id=user_id
+        )
+
+        if not instance:
+            instance = await self.repository.get_box_instance_by_id(
+                user_id=user_id,
+                instance_id=instance_id
+            )
+
+        if instance and instance.shelf_id:
+            await self.shelf_cache_manager.delete_shelf(
+                user_id=user_id,
+                shelf_id=instance.shelf_id,
+            )
 
 
     async def move_to_shelf(
@@ -34,12 +81,34 @@ class InstanceManager:
         user_id: uuid.UUID,
         instance_id: uuid.UUID,
         shelf_id: uuid.UUID | None,
-    ) -> BoxInstanceReturn:
-        return await self.repository.move_to_shelf(
+    ) -> BoxInstanceWithTemplate:
+        await self.refresh_old_shelf(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
+
+        await self.instance_cache_manager.delete_instance(
+            instance_id=instance_id,
+            user_id=user_id,
+        )
+
+        updated_instance = await self.repository.move_to_shelf(
             user_id=user_id,
             shelf_id=shelf_id,
             instance_id=instance_id,
         )
+
+        if shelf_id:
+            await self.shelf_cache_manager.delete_shelf(
+                user_id=user_id,
+                shelf_id=shelf_id,
+            )
+
+        await self.instance_cache_manager.store_instance(
+            instance=updated_instance
+        )
+
+        return updated_instance
 
 
     async def get_instances(
@@ -55,11 +124,25 @@ class InstanceManager:
         self,
         user_id: uuid.UUID,
         instance_id: uuid.UUID,
-    ) -> BoxInstanceWithTemplate:
-        return await self.repository.get_box_instance_by_id(
+    ) -> BoxInstanceWithTemplate | None:
+        cached = await self.instance_cache_manager.get_instance(
             user_id=user_id,
             instance_id=instance_id,
         )
+        if cached:
+            return cached
+
+        instance = await self.repository.get_box_instance_by_id(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
+
+        if instance:
+            await self.instance_cache_manager.store_instance(
+                instance=instance
+            )
+
+        return instance
 
 
     async def delete_instance(
@@ -67,7 +150,17 @@ class InstanceManager:
         user_id: uuid.UUID,
         instance_id: uuid.UUID,
     ) -> None:
-        return await self.repository.delete_box_instance(
+        await self.refresh_old_shelf(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
+
+        await self.repository.delete_box_instance(
+            user_id=user_id,
+            instance_id=instance_id,
+        )
+
+        await self.instance_cache_manager.delete_instance(
             user_id=user_id,
             instance_id=instance_id,
         )
